0cdeda09f086318c7af51cf338690364d9970e2f
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-20 12:21:34 +0300

    CREATE city repository add method read, update and delete

--- src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
+++ src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
@@ -5,4 +5,14 @@ public class CityRepository {
     public String create(){
         return "Create City";
     }
+    public String read(){
+        return "Read City";
+    }
+    public String update(){
+        return "Update City";
+    }
+
+    public String delete(){
+        return "Delete City";
+    }
 }
