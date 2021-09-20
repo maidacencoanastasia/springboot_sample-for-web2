50862026b75ac9d36f7799af099381ed5f649f9f
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-20 12:38:58 +0300

    add list of cities and create constructor

--- src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
+++ src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
@@ -1,6 +1,17 @@
 package com.springboot_sample.springboot_sample.repository;
 
+import java.util.ArrayList;
+
 public class CityRepository {
+
+    ArrayList<String> cities = new ArrayList<>();
+
+    public CityRepository(){
+        this.cities.add("Balti");
+        this.cities.add("Kiev");
+        this.cities.add("Iasi");
+        this.cities.add("Stambul");
+    }
 
     public String create(String cityName){
         return "City created";
