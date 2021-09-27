affcd403206cac0c41d2bfa045776880f8f1868d
maidacencoanastasia <maidacenco100@gmail.com>
2021-09-27 12:40:05 +0300

    fixed

--- src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
+++ src/main/java/com/springboot_sample/springboot_sample/repository/CityRepository.java
@@ -9,10 +9,10 @@ public class CityRepository {
 
     ArrayList<City> cities = new ArrayList<>();
 
-    public CityRepository(){
-        }
+    public CityRepository() {
+    }
 
-    public String create(String CityName, Integer countryId,String countryName){
+    public String create(String CityName, Integer countryId, String countryName) {
         City city = new City(CityName, countryId, countryName);
         this.cities.add(city);
         return "City created";
@@ -22,12 +22,12 @@ public class CityRepository {
         return this.cities.toString();
     }
 
-    public String read(Integer id){
+    public String read(Integer id) {
         try {
             City City = this.cities.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
             assert City != null;
             return City.toString();
-        } catch (Exception exception){
+        } catch (Exception exception) {
             return "Not found";
         }
     }
@@ -38,22 +38,24 @@ public class CityRepository {
             assert city != null;
             city.setName(cityName);
             return "City updated";
-        } catch (Exception exception){
+        } catch (Exception exception) {
             return "City not found";
         }
     }
-    public String delete(Integer id){
-    try {
-        City city = this.cities.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
-        if(city == null){
-            return "City not found";            }
-        this.cities.remove(city);
-        return "City deleted";
-    } catch (Exception exception){
-        return "City not found";
-    }
 
-}
+    public String delete(Integer id) {
+        try {
+            City city = this.cities.stream().filter(element -> id.equals(element.getId())).findFirst().orElse(null);
+            if (city == null) {
+                return "City not found";
+            }
+            this.cities.remove(city);
+            return "City deleted";
+        } catch (Exception exception) {
+            return "City not found";
+        }
+
+    }
 
 
 }
